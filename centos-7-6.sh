9988c571ef5fe6a5fb5c071cc98576b2a68b215a
Steven Marshall <steven.marshall@ubc.ca>
2020-01-22 12:46:17 -0800

    Replaced ADMINER_VERSION with 'latest'.

--- centos-7-6.sh
+++ centos-7-6.sh
@@ -56,7 +56,7 @@ echo '==> Installing Adminer'
 
 if [ ! -d /usr/share/adminer ]; then
     mkdir -p /usr/share/adminer/plugins
-    curl -LsS https://github.com/vrana/adminer/releases/download/v$ADMINER_VERSION/adminer-$ADMINER_VERSION.php -o /usr/share/adminer/adminer-$ADMINER_VERSION.php
+    curl -LsS https://www.adminer.org/latest-en.php -o /usr/share/adminer/adminer-latest.php
     curl -LsS https://raw.githubusercontent.com/vrana/adminer/master/plugins/plugin.php -o /usr/share/adminer/plugins/plugin.php
     curl -LsS https://raw.githubusercontent.com/vrana/adminer/master/plugins/login-password-less.php -o /usr/share/adminer/plugins/login-password-less.php
     curl -LsS https://raw.githubusercontent.com/vrana/adminer/master/designs/nicu/adminer.css -o /usr/share/adminer/adminer.css
@@ -80,8 +80,6 @@ sed -i 's#PORT_80#'$PORT_80'#' /etc/httpd/conf.d/virtualhost.conf
 cp $CONFIG_PATH/adminer.conf /etc/httpd/conf.d/adminer.conf
 sed -i 's#PORT_80#'$PORT_80'#' /etc/httpd/conf.d/adminer.conf
 cp $CONFIG_PATH/adminer.php /usr/share/adminer/adminer.php
-ESCAPED_ADMINER_VERSION=`echo $ADMINER_VERSION | sed 's/\./\\\\./g'`
-sed -i 's#ADMINER_VERSION#'$ESCAPED_ADMINER_VERSION'#' /usr/share/adminer/adminer.php
 
 # PHP.ini
 cp $CONFIG_PATH/php.ini.htaccess /var/www/.htaccess
@@ -108,4 +106,3 @@ echo $(mysql -V)
 echo $(php -v | head -n1)
 echo $(python --version)
 echo $(python3 --version)
-echo Adminer $ADMINER_VERSION
